bbcc6823f7d421df06e1ddfcaa27d4c2080b54c3
Amy Chen <ccircle01999@gmail.com>
2026-05-21 13:24:41 -0400

    "completed stampbadge "

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import StampBadge from "./components/Stampbadge";
 import ThemeShowcase from "./components/ThemeShowcase";
 
 function App() {
-  return <ThemeShowcase />
-  // return <h1 className="text-3xl font-display underline">Hello world!</h1>
+  return (
+    <div>
+      <ThemeShowcase />
+      <StampBadge />
+    </div>
+  );
 }
 
 export default App
